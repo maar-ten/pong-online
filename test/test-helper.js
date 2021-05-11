0518c10463cef45446c9856c309ecca61fdb8444
maar-ten <maar-ten@users.noreply.github.com>
2021-05-11 14:27:47 +0000

    Add more dependencies for Chromium

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,8 +1,12 @@
 import puppeteer from 'puppeteer';
 
-export default async function () {
+export default async function (t, run) {
 	const browser = await puppeteer.launch({
-		args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        headless: true,
+        args: [ // Disable Chromium's unnecessary SUID sandbox.
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ]
 	});
 	const page = await browser.newPage();
 	try {

496f4dc877172f40ba7b9a6c91100819f1a9820d
maar-ten <maar-ten@users.noreply.github.com>
2021-05-13 11:25:34 +0000

    Add screenshot test for welcome page

--- test/game-page.test.js
+++ test/game-page.test.js
@@ -1,4 +1,5 @@
 import test from 'ava';
+import ScreenshotTester from 'puppeteer-screenshot-tester';
 import testHelper from './puppeteer-test-helper.js';
 
 const START_PAGE_URL = 'http://localhost:3000';
@@ -8,8 +9,11 @@ test('title should be: This is Pong!', testHelper, async (t, page) => {
     t.is(await page.title(), 'This is Pong!');
 });
 
-test.skip('starting page renders the same as previous version', testHelper, async (t, page) => {
+test.skip('welcome screen should look like it did in the previous version', testHelper, async (t, page) => {
+    const tester = await ScreenshotTester();
     await page.goto(START_PAGE_URL);
-    // await page.screenshot({ path: 'example.png' });
-    // todo implement
+
+    const result = await tester(page, 'welcome-screen');
+
+    t.true(result);
 });

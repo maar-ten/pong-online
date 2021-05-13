2a5eeb2fa83261373b3a20d67ca9a716ebde3de2
maar-ten <maar-ten@users.noreply.github.com>
2021-05-13 18:44:19 +0000

    Remove screenshot test and add test for canvas element

--- test/game-page.test.js
+++ test/game-page.test.js
@@ -1,19 +1,16 @@
 import test from 'ava';
-import ScreenshotTester from 'puppeteer-screenshot-tester';
 import testHelper from './puppeteer-test-helper.js';
 
 const START_PAGE_URL = 'http://localhost:3000';
 
 test('title should be: This is Pong!', testHelper, async (t, page) => {
     await page.goto(START_PAGE_URL);
+    
     t.is(await page.title(), 'This is Pong!');
 });
 
-test.skip('welcome screen should look like it did in the previous version', testHelper, async (t, page) => {
-    const tester = await ScreenshotTester();
+test('should contain a canvas element', testHelper, async (t, page) => {
     await page.goto(START_PAGE_URL);
 
-    const result = await tester(page, 'welcome-screen');
-
-    t.true(result);
+    t.not(await page.$('canvas'), null);
 });
